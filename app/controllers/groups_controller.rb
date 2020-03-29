5ed96c20454a3455f97c2bfe943daed032bf1408
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-03-29 17:22:17 +0900

    コントロラー (作成するグループのデータを保存)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,29 @@
 class GroupsController < ApplicationController
 
   def new
+    #空のインスタンスを作成
+    @group = Group.new
+    #ログイン中のユーザーは、作成するグループに追加する
+    @group.users << current_user
   end
 
   def create
+    #グループ作成時 フォームからの送信されるデータを確認しておく
+    #binding.pry
+
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      #newページの表示。リダイレクトすると変数が消えてしまう
+      render :new
+    end
+  end
+
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 
 end
