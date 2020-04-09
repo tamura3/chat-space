4e19e2425beea75b59519808df0a07cfca9d5531
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-04-09 22:32:43 +0900

    reloadMessages関数で最新メッセージを取得

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -72,7 +72,14 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.mainChat__content').append(insertHTML);
     })
     .fail(function() {
       alert('error');
