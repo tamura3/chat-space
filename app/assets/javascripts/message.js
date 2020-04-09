3bccaed2ad1be0b4e68560d3c82206dc721d85c3
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-04-09 21:57:04 +0900

    reloadMessages関数の作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,4 @@
 $(function(){ 
-  var last_message_id = $('.mainChat__content__message:last').data("message-id");
-  console.log(last_message_id);
     function buildHTML(message){
       if ( message.image ) {
         var html =
@@ -37,7 +35,7 @@ $(function(){
         return html;
       };
     }
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -59,5 +57,26 @@ $('#new_message').on('submit', function(e){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-  })
+  });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.mainChat__content__message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  
 });
